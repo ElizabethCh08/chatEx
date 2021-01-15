1338c00119d3718cd92c9c4bd94756eb7ffafcf7
ElizabethCh <karlita-ch8@outlook.es>
2021-01-15 16:25:40 -0500

    Autenticación y chat

---
+++ chatEx/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+      apiKey: "AIzaSyCTvnMiwt6ZvVR6zj7kXUS8Dv3ibH8xiQ0",
+      authDomain: "chatex-8c38e.firebaseapp.com",
+      projectId: "chatex-8c38e",
+      storageBucket: "chatex-8c38e.appspot.com",
+      messagingSenderId: "187229878721",
+      appId: "1:187229878721:web:4b9a4ef49b394f97f95740",
+      measurementId: "G-ST2NE10EYC"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
